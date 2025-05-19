82e56638ca2c6bf47c8f86aa9dec989bdd91bf17
Jeong-dap <njd990603@naver.com>
2025-05-19 20:07:14 +0900

    최종수정

--- src/main/java/com/example/demo/Tour/TourRepository.java
+++ src/main/java/com/example/demo/Tour/TourRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository // Spring의 Repository 컴포넌트임을 알려줌 (빈으로 등록)
